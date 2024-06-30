50680ae9ebd2ca43169a8788ab3173577002444b
Sujay2006 <sujaykumarshah550@gmail.com>
2024-06-30 19:19:12 +0530

    Update CallList.tsx

--- src/components/CallList.tsx
+++ src/components/CallList.tsx
@@ -3,7 +3,7 @@
 import { Call, CallRecording } from '@stream-io/video-react-sdk';
 
 import Loader from './Loader';
-import { useGetCalls } from '@/hooks/useGetCalls';
+import { useGetCalls } from '../../hooks/useGetCalls'
 import MeetingCard from './MeetingCard';
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
