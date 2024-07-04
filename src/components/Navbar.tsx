e6eb4d5af4e28b294fc56160beae8923b2d3ddd5
Sujay2006 <sujaykumarshah550@gmail.com>
2024-07-04 12:57:57 +0530

    Update Navbar.tsx
    images

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   return (
     <nav className='flex justify-between fixed z-50 w-full bg-dark-1 px-10 py-5 lg:px10 '>
       <Link href='/' className='flex items-center gap-1'>
-        <Image src="/icons/logo.svg" alt='yoom logo' width={40} height={40} className='max-sm:size-10'/>
+        <Image src="/images/sujay.png" alt='yoom logo' width={40} height={40} className='max-sm:size-10'/>
         <p className='text-[26px] font-extrabold text-white max-sm:size-10'>Yoom</p>
       </Link>
 
